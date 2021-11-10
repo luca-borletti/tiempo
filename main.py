33eeae25da8ec47051a863546876cfa52ad86aa7
Luca Borletti <lucaborletti@lucas-mbp.wifi.local.cmu.edu>
2021-11-09 22:13:35 -0500

    added all controller inputs from 112 website

---
+++ main.py
@@ -0,0 +1,36 @@
+from cmu_112_graphics import *
+
+def appStarted(app):
+    pass
+
+def timerFired(app):
+    pass
+
+def appStopped(app):
+    pass
+
+def keyPressed(app, event):
+    pass
+
+def keyReleased(app, event):
+    pass
+
+def mousePressed(app, event):
+    pass
+
+def mouseReleased(app, event):
+    pass
+
+def mouseMoved(app, event):
+    pass
+
+def mouseDragged(app, event):
+    pass
+
+def sizeChanged(app):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+runApp(width=600, height=600)
